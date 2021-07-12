da5cf4cdd745c1a384b84f7c7c185146b6556690
meggouri ismail <meggouri.ismail@gmail.com>
2021-07-12 12:42:16 +0100

    Add project files.

---
+++ 7Club/Consomation.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace _7Club
+{
+    partial class Consomation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AutoArrange = false;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(550, 137);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(238, 234);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(738, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "-";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.AutoArrange = false;
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(12, 12);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(532, 174);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView3
+            // 
+            this.listView3.AutoArrange = false;
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(12, 192);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(532, 280);
+            this.listView3.TabIndex = 4;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(582, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 29);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "VIP";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(618, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 42);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "7CLUB";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label2.Location = new System.Drawing.Point(674, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "AHMAD";
+            // 
+            // Consomation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 484);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listView3);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Consomation";
+            this.Text = "Consomation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ 7Club/Consomation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7Club
+{
+    public partial class Consomation : Form
+    {
+        public Consomation()
+        {
+            InitializeComponent();
+            listView1.Items.Add("                          7CLUB ");
+            listView1.Items.Add("");
+            listView1.Items.Add("");
+            listView1.Items.Add("Nom Produit        10.00DH");
+            listView1.Items.Add("Nom Produit        10.50DH");
+            listView1.Items.Add("Nom Produit        11.00DH");
+            listView1.Items.Add("Nom Produit        12.00DH");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listView1.Items.Count - 1 >= 0)
+            {
+                listView1.Items.RemoveAt(listView1.Items.Count - 1);
+            }
+        }
+    }
+}
